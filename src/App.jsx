4cc8e5cbd3ccf55e1b035004d4171cff089e56b5
Piyush Verma <piyushverma2001@gmail.com>
2024-06-20 12:10:56 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Header } from './components/Header.jsx';
+import { Balance } from './components/Balance.jsx';
+import { IncomeExpenses } from './components/IncomeExpenses.jsx';
+import { TransactionList } from './components/TransactionList.jsx';
+import { AddTransaction } from './components/AddTransaction.jsx';
+import { GlobalProvider } from "./context/GlobalState.jsx";
+import './App.css';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
